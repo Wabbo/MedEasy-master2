2676c085f5bea1aa67f641ddbb1685316283fadf
Wabbo <Wabbo@users.noreply.github.com>
2019-01-26 23:08:00 +0200

    V2

--- app/src/main/java/com/example/nihal/medeasy/MainActivity.java
+++ app/src/main/java/com/example/nihal/medeasy/MainActivity.java
@@ -1,17 +1,59 @@
 package com.example.nihal.medeasy;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText loginPhone , loginPassWord ;
+    Button login ;
+    FirebaseAuth auth = FirebaseAuth.getInstance() ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginPhone = findViewById(R.id.loginPhone) ;
+        loginPassWord =findViewById(R.id.loginPassWord) ;
+        login =findViewById(R.id.login) ;
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String UserPhoneInFireB = loginPhone.getText().toString()+"@gmail.com" ;
+                String PassWordInFireB = loginPassWord.getText().toString();
+
+                auth.signInWithEmailAndPassword(UserPhoneInFireB,PassWordInFireB).addOnCompleteListener(MainActivity.this , new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            FirebaseUser user = task.getResult().getUser() ;
+                            startActivity(new Intent(MainActivity.this,Home.class));
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this," Phone or PassWord are incorrect " ,Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
 
 
 
--- app/src/main/java/com/example/nihal/medeasy/signup.java
+++ app/src/main/java/com/example/nihal/medeasy/signup.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -12,16 +13,20 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class signup extends AppCompatActivity {
 
-    private FirebaseAuth auth ;
-    FirebaseDatabase database ;
-    DatabaseReference myRef ;
 
-    EditText UserName , Phone , Address , Occupation , FamilyHistoryLink , Weight , Height , PassWord ;
+    EditText UserName, Phone, Address, Occupation, FamilyHistoryLink, Weight, Height, PassWord;
+    Button saveData;
+
+    FirebaseAuth auth;
+
+    //  FirebaseDatabase database  ;
+    // DatabaseReference myRef ;
 
 
     @Override
@@ -29,49 +34,83 @@ public class signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        auth = FirebaseAuth.getInstance() ;
 
+        UserName = findViewById(R.id.UserName);
+        Phone = findViewById(R.id.Phone);
+        Address = findViewById(R.id.Address);
+        Occupation = findViewById(R.id.Occupation);
+        FamilyHistoryLink = findViewById(R.id.FamilyHistoryLink);
+        Weight = findViewById(R.id.Weight);
+        Height = findViewById(R.id.Height);
+        PassWord = findViewById(R.id.PassWord);
+        saveData = findViewById(R.id.SignUp);
 
-        UserName = findViewById(R.id.UserName) ;
-        Phone = findViewById(R.id.Phone) ;
-        Address = findViewById(R.id.Address) ;
-        Occupation = findViewById(R.id.Occupation) ;
-        FamilyHistoryLink = findViewById(R.id.FamilyHistoryLink) ;
-        Weight = findViewById(R.id.Weight) ;
-        Height = findViewById(R.id.Height) ;
-        PassWord = findViewById(R.id.PassWord) ;
+        auth = FirebaseAuth.getInstance();
+        //  database  = FirebaseDatabase.getInstance();
+        // myRef = database.getReference("Users");
 
-    }
-
-    public void SignUpOnClick ( View view ) {
-
-        String UserNameInFireB = UserName.getText().toString() ;
-        String PassWordInFireB = PassWord.getText().toString();
-
-        database  = FirebaseDatabase.getInstance();
-        myRef = database.getReference("TestDataBase") ;
-
-        auth.createUserWithEmailAndPassword(UserNameInFireB,PassWordInFireB).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-
+        saveData.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
+            public void onClick(View view) {
+                String UserPhoneInFireB = Phone.getText().toString() + "@gmail.com";
+                String PassWordInFireB = PassWord.getText().toString();
+                String UserNameInFireB = UserName.getText().toString();
+                String AddressInFireB = Address.getText().toString();
+                String OccupationInFireB = Occupation.getText().toString();
+                String FamilyHistoryLinkInFireB = FamilyHistoryLink.getText().toString();
+                String WeightInFireB = Weight.getText().toString();
+                String HeightInFireB = Height.getText().toString();
 
-                if (task.isSuccessful()){
-                    myRef.setValue(Phone.getText().toString().trim());
-                    myRef.setValue(Address.getText().toString().trim());
-                    myRef.setValue(Occupation.getText().toString().trim());
-                    myRef.setValue(FamilyHistoryLink.getText().toString().trim());
-                    myRef.setValue(Weight.getText().toString().trim());
-                    myRef.setValue(Height.getText().toString().trim());
+                final User user = new User(
+                        UserNameInFireB,
+                        AddressInFireB,
+                        OccupationInFireB,
+                        FamilyHistoryLinkInFireB,
+                        WeightInFireB,
+                        HeightInFireB
+                );
 
-                    Toast.makeText(signup.this,"Successful ",Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Toast.makeText(signup.this,"NotSuccessful ",Toast.LENGTH_SHORT).show();
-                }
-                finish();
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                auth.createUserWithEmailAndPassword(UserPhoneInFireB, PassWordInFireB).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            /*FirebaseUser firebaseUser = task.getResult().getUser();
+                            String ref = firebaseUser.getUid();
+                            myRef.child(ref).setValue(user);
+                            */
+                            FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getUid()).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(signup.this, " Register Successfull  ", Toast.LENGTH_SHORT).show();
+                                        finish();
+                                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                    } else {
+                                        if (task.getException().getMessage().equals("The email address is already in use by another account.")) {
+                                            Toast.makeText(signup.this, "The phone is already in use by another user.", Toast.LENGTH_SHORT).show();
+                                        } else
+                                            Toast.makeText(signup.this, " Register Fail", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
+
+                        } else {
+                            if (task.getException().getMessage().equals("The email address is already in use by another account.")) {
+                                Toast.makeText(signup.this, "The phone is already in use by another user.", Toast.LENGTH_SHORT).show();
+                            } else
+                                Toast.makeText(signup.this, " Register Fail", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
             }
         });
+
     }
+
 }
+
